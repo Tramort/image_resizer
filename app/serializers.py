9714b5605521f487d4c904b5b38a63d72a074434
Tramort <jake@gogo.by>
2016-09-30 21:03:56 +0300

    app/serializers.py: fixed datetime format.
    True way handle datetime on client, but in another time ;)

--- app/serializers.py
+++ app/serializers.py
@@ -9,6 +9,11 @@ class ResizeTaskSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializing all ResizeTasks
     """
+    receive_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",
+                                             required=False, read_only=True)
+    converted_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S",
+                                               required=False, read_only=True)
+
     class Meta:
         model = models.ResizeTask
         fields = ("id", "receive_time", "image", "resized_image", "converted_time",)
